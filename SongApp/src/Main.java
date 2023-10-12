1c19cd2d26353d69c8733183ad51a8d1803e75c3
SirUnity156 <131442618+SirUnity156@users.noreply.github.com>
2023-10-12 10:04:03 +0100

    Add files via upload

--- SongApp/src/Main.java
+++ SongApp/src/Main.java
@@ -10,6 +10,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    //Global Scanner variable
+    //Wanted to avoid using global variables wherever possible but iterating over a local context containing a Scanner definition and references leads to issues regarding input reading
+    //Global Scanner also prevents the need for repeated declaration and de-allocation lines
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         //File path and file object instantiation
         Path path = Paths.get("SongList.txt");
@@ -18,7 +22,7 @@ public class Main {
         //Creates a file if it isn't already present
         file.createNewFile();
 
-        while(true) { //I know while(true) is a bit naughty, but it works and doesn't cause any uncontrolled iteration as the loop awaits user input for on every iteration
+        while(true) { //I know while(true) is a bit naughty, but it works and doesn't cause any uncontrolled iteration as the loop awaits user input on every iteration
             //Read all lines
             List<Song> lines = getLines(file, path);
 
@@ -30,8 +34,6 @@ public class Main {
      * If command isn't recognised, it informs the user.
     */
     public static void takeCommand(List<Song> lines, Path path) {
-        //Quick explanation for going forward: Yes, I do make a new Scanner object in each context but I wanted to avoid using global variables as much as possible. Each Scanner shall be closed to avoid memory leak.
-        Scanner sc = new Scanner(System.in);
         System.out.println("Type \"help\" for command list");
         System.out.print(">> "); //Shows the user where to type, aesthetic choice
         String input = sc.nextLine();
@@ -69,7 +71,6 @@ public class Main {
                 System.out.println("Command not recognised");
                 break;
         }
-        sc.close();
     }
 
     /**Reads all lines from the file and saves them to a Song list to be returned.
@@ -80,7 +81,7 @@ public class Main {
         List<Song> songList = new ArrayList<Song>();
         if(!lines.isEmpty()) {
             for (String line : lines) {
-                songList.add(new Song(line));
+                songList.add(makeSongFromInput(line));
             }
         }
         return songList;
@@ -129,7 +130,6 @@ public class Main {
 
     /**Prints all songs over specified play threshold.*/
     public static void playsOver(List<Song> lines) {
-        Scanner sc = new Scanner(System.in);
         boolean isntInt;
         int num;
         //Loops until valid input
@@ -137,7 +137,7 @@ public class Main {
             num = 0;
             isntInt = false; // Used for validation
             System.out.println("Enter minimum plays");
-            System.out.print(">>> ");
+            System.out.print(">> ");
             String input = sc.nextLine();
             //Input validation
             try {num = Integer.parseInt(input);}
@@ -147,21 +147,19 @@ public class Main {
             }
         } while(isntInt);
         printSongsOverNum(lines, num);
-        sc.close();
     }
 
     /**Adds a song with specified details to the file.*/
     public static void add(List<Song> lines, Path path) {
-        Scanner sc = new Scanner(System.in);
         boolean validInput; //For validation
         do { //Loops until valid input
             validInput = true;
             System.out.println("Enter song details in following format: name, artist, plays");
-            System.out.print(">>> ");
+            System.out.print(">> ");
             String song = sc.nextLine();
             //Input Validation
             try {
-                lines.add(new Song(song));
+                lines.add(makeSongFromInput(song));
             }
             catch (IOException e) {
                 validInput = false;
@@ -176,7 +174,6 @@ public class Main {
         try {updateFile(lines, path);}
         catch(IOException ignored){}
         System.out.println("Song added");
-        sc.close();
     }
 
     /**Removes a specified song from the song list
@@ -184,11 +181,10 @@ public class Main {
      * If specified song isn't found, it loops and reprompts the user.
     */
     public static void remove(List<Song> lines, Path path) {
-        Scanner sc = new Scanner(System.in);
         boolean found = false; //Can you guess what needs to happen for this to become true?
         do { //Loops until valid input
             System.out.println("Enter song name");
-            System.out.print(">>> ");
+            System.out.print(">> ");
             String line = sc.nextLine();
             //Loops through to see if any songs matching the input are stored, then removes it.
             for (Song thisSong : lines) {
@@ -207,6 +203,17 @@ public class Main {
         try {updateFile(lines, path);}
         catch(IOException ignored){}
         System.out.println("Song removed");
-        sc.close();
+    }
+    
+    /**Takes string input, splits it and sets the fields*/
+    public static Song makeSongFromInput(String line) throws IOException, NumberFormatException {
+        //Split into name, artist and plays
+        //Rest of validation for this section is done at function call
+        String[] details = line.split(", ");
+        //Rest of validation for this section is done at function call
+        if(details.length != 3) throw new IOException(); //Detects if user has inputted data incorrectly, I have decided to throw an IOException specifically since Exception is too broad and may lead to unintended exception catching
+        //Applies values
+        return new Song(details[0], details[1], Integer.parseInt(details[2]));
+        //If integer parsing fails, the NumberFormatException is caught and the user is notified of their severe lapse in judgement
     }
 }
--- SongApp/src/Song.java
+++ SongApp/src/Song.java
@@ -1,33 +1,29 @@
-import java.io.IOException;
-
 public class Song {
-    //Song attributes set to private as implementation of encapsulation to prevent accidental editing and increase maintainability
+    //Song fields set to private as implementation of encapsulation to prevent accidental editing and increase maintainability
     //Final as, after initialisation, they will have no requirement to change
     private final String name;
     private final String artist;
     private final int plays;
 
-    /**Constructor takes string input, splits it and sets the attributes*/
-    public Song(String line) throws IOException, NumberFormatException {
-        //Split into name, artist and plays
-        String[] details = line.split(", ");
-        //Rest of validation for this section is done in the main class at instantiation
-        if(details.length != 3) throw new IOException(); //Detects if user has inputted data incorrectly, I have decided to throw an IOException specifically since Exception is too broad and may lead to unintended exception catching
-        //Applies values
-        this.name = details[0];
-        this.artist = details[1];
-        this.plays = Integer.parseInt(details[2]); //If this fails, the NumberFormatException is caught and the user is notified of their severe lapse in judgement
+    /**Constructor takes input values and sets the fields*/
+    public Song(String name, String artist, int plays) {
+        this.name = name;
+        this.artist = artist;
+        this.plays = plays;
     }
 
     //Getters
+    /**Returns name */
     public String getName() {
         return name;
     }
 
+    /**Returns artist */
     public String getArtist() {
         return artist;
     }
 
+    /**Returns plays */
     public int getPlays() {
         return plays;
     }
